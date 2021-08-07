41a2e130c3cb63a1e6b0ed9a73b44a8f4f56014c
leonardo mosso <leonardo_mcv@hotmail.com>
2021-08-07 11:38:18 -0300

    alteracoes imagens e layout

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,96 +1,57 @@
 import React from 'react';
 import CardProduto from './Produtos';
-
-// function NumeroItem () {
-//     return <div> NUmero de iems</div>
-// }
-// function Ordenacao (){
-//     return <div>ORDEM</div>
-// }
-
-// function Cabecalho () {
-//     return <div>
-//         <NumeroItem/>
-//          <Ordenacao/>
-//     </div> 
-// }
+import produto1 from '../img/produto1.jpg';
+import produto2 from '../img/produto2.jpg';
+import produto3 from '../img/produto3.jpg';
+import produto4 from '../img/produto4.jpg';
+import produto5 from '../img/produto5.jpg';
 
 class Main extends React.Component {
-    state = {
-      produtos: [
-        {
-          id: 1,
-          name: "Foguete da Missão Apollo 11",
-          value: 10000.0,
-          imageUrl: "https://picsum.photos/200/200"
-        },
-        {
-          id: 2,
-          name: "Sputnik",
-          value: 500000.0,
-          imageUrl: "https://picsum.photos/200/200"
-        },
-        {
-          id: 3,
-          name: "Starlink",
-          value:400000.0,
-          imageUrl: "https://picsum.photos/200/200"
-        },
-        {
-            id: 3,
-            name: "Starlink",
-            value:50000.0,
-            imageUrl: "https://picsum.photos/200/200"
-          },
-          {
-            id: 3,
-            name: "Starlink",
-            value:100.0,
-            imageUrl: "https://picsum.photos/200/200"
-          }
-  
-      ],
-      // valorOrdem: ""
-    };
+  state = {
+    produtos: [
+      {
+        id: 1,
+        name: "Foguete da Missão Apollo 11",
+        value: 59,
+        imageUrl: produto1
+      },
+      {
+        id: 2,
+        name: "Sputnik",
+        value: 49,
+        imageUrl: produto2
+      },
+      {
+        id: 3,
+        name: "Starlink",
+        value: 39,
+        imageUrl: produto3
+      },
+      {
+        id: 4,
+        name: "Starlink",
+        value: 49,
+        imageUrl: produto4
+      },
+      {
+        id: 5,
+        name: "Camiseta Nasa, mangas longas",
+        value: 69,
+        imageUrl: produto5
+      }
+
+    ],
     
-    // onChangeOrdem = (event) => {
-    //   this.setState({valorOrdem: event.target.value})
-    // }
-
-    
+  };
+  render() {
 
 
-    render() {
-     
-      
-      return (
-        <div className="App">
-          {/* <div>
-          <h4>Ordenação: </h4>
-          <select value={this.state.valorOrdem} onChange={this.onChangeOrdem}>
-            <option value=""></option>
-            <option value="decrescente">Decrescente</option>
-            <option value="crescente">Crescente</option>
-          </select>
-          </div> */}
-          <CardProduto produto={this.state.produtos} />
-        </div>
-      );
-    }
+    return (
+      <div className="App">
+        <CardProduto produto={this.state.produtos} />
+      </div>
+    );
   }
-  
- 
+}
 
-// function Main () {
-//     return (
-//         <div>
-//               {/* <Cabecalho/>
-//               <section className='GridProd'>
-//               <Produtos/>
-//               <Produtos/>
-//               <Produtos/>
-//               </section> */}
-//         </div>
-//     )
-// }
 export default Main;
--- src/component/Produtos.js
+++ src/component/Produtos.js
@@ -1,99 +1,106 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Produtos = styled.div `
-    display: grid;
+const Produtos = styled.div`
+  display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 16px;
   padding: 16px;
   border: 1px solid black;
+`
+
+const CardIndividual = styled.div`
+  border: 1px solid black;
   
-       
+
+`
+
+const Img = styled.img`
+  width: 250px;
+  height: 300px;
+  
+  
+
+`
+const TituloCamiseta = styled.p`
+  text-align: center;
 
 
 `
 
-const CardIndividual = styled.div `
-    border: 1px solid black;
+const Botao = styled.button `
+   
+   width:100%;
+   &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+    
+  }
 
 `
+const DivOrdenacao = styled.div `
+  display:flex;
+  align-items:center;
+  justify-content: space-between;
+  
+`
+
+
 
 class CardProduto extends React.Component {
-  state = { 
+  state = {
     valorOrdem: ""
-  
+
   }
 
   onChangeOrdem = (event) => {
-    this.setState({valorOrdem: event.target.value})
+    this.setState({ valorOrdem: event.target.value })
   }
 
   listaFiltrada = () => {
-    return this.props.produto.sort((a,b) => this.state.valorOrdem === "crescente" ? a.value - b.value : b.value - a.value)
+    return this.props.produto.sort((a, b) => this.state.valorOrdem === "crescente" ? a.value - b.value : b.value - a.value)
   }
 
 
-  
-  render(){
+
+  render() {
     console.log(this.props.produto)
     console.log(this.state.valorOrdem)
     const listaDeProdutos = this.listaFiltrada()
-    
-  return (
-    <div>
-        <div>
+
+    return (
+      <div>
+        <DivOrdenacao>
+          <div>
+          <p><strong>Quantidade de produtos:</strong> {this.props.produto.length}</p>
+          </div>
+          <DivOrdenacao>
           <h4>Ordenação: </h4>
           <select value={this.state.valorOrdem} onChange={this.onChangeOrdem}>
             <option value=""></option>
             <option value="decrescente">Decrescente</option>
             <option value="crescente">Crescente</option>
           </select>
-          </div>
-    <Produtos>
-      
-      {listaDeProdutos.map((prod) =>{
-      return (
+          </DivOrdenacao>
+        </DivOrdenacao>
+        <Produtos>
 
-        <CardIndividual>
-        <img src={prod.imageUrl} alt="" />
-        {prod.name}
-        <p> Valor: {prod.value},00 </p>
-        <button >Adicionar ao carrinho</button>
-        </CardIndividual>
+          {listaDeProdutos.map((prod) => {
+            return (
 
-      )
-    })}
-    </Produtos>
-    </div>
-  );
+              <CardIndividual>
+                <Img src={prod.imageUrl} alt="" />
+                <TituloCamiseta><strong>{prod.name}</strong></TituloCamiseta>
+                <p> Valor: {prod.value},00 </p>
+                <Botao >Adicionar ao carrinho</Botao>
+              </CardIndividual>
+
+            )
+          })}
+        </Produtos>
+      </div>
+    );
+  };
 };
-};
 
-
-
-
-
-
-
-// function BoxProdutos () {
-//    return(<div className="Mainbox">
-     
-//     <h1>Produto</h1>
-//     <h2>Valor</h2>
-//     <button>Comprar</button>
-//     </div>) 
-// }
-
-
-
-
-
-
-// function Produtos () {
-//     return (
-//         <div>
-//             <BoxProdutos/>
-//         </div>
-//     )
-// }
 export default CardProduto;
